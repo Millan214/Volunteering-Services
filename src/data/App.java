784e3d0b360a9e541688060302a1176c3d232363
Millan214 <63602414+Millan214@users.noreply.github.com>
2020-05-15 18:05:47 +0200

    cambio main

---
+++ src/data/App.java
@@ -0,0 +1,39 @@
+package data;
+
+import gui.VenOpcUsr;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import utilsFicheros.FicUtls;
+
+public class App {
+    /**
+     * @author Millan & Abel
+     * @param args
+     * @throws java.io.IOException
+     */
+    public App() throws IOException {
+        
+        File fasoc = new File("src"+File.separator+"ficheros"+File.separator+"asociaciones.txt");
+        ArrayList<Asociacion> asociaciones = new ArrayList<>();
+        deFicheroAListaAsoc(fasoc, asociaciones);
+
+        VenOpcUsr v = new VenOpcUsr(asociaciones);
+        v.setVisible(true);
+        
+        
+    }
+    
+    /**
+     * @param fasoc Fichero de asociaciones
+     * @param asociaciones Lista de asociaciones a la que quieres pasar lo que hay en el fichero
+     * @throws IOException
+     */
+    private void deFicheroAListaAsoc(File fasoc, ArrayList<Asociacion> asociaciones) throws IOException {
+        FicUtls fic = new FicUtls();
+        String str = fic.leer(fasoc).substring(0, fic.leer(fasoc).indexOf("*"));
+        String[] strpar = str.split(",");
+        asociaciones.add(new Asociacion(strpar[0], strpar[1]));
+    }
+    
+}
